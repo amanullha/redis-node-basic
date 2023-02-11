3ec0a494c31fd9f3db85bfa0cb9bd5452c123260
amanullha <aullah060@gmail.com>
2023-02-11 19:50:27 +0600

    updated redis connection setup

--- index.js
+++ index.js
@@ -1,124 +1,56 @@
-const util = require("util")
-const express = require("express")
-const app = express()
-app.use(express.json())
+const redis = require("redis");
+const url = 'redis://alice:foobared@awesome.redis.server:6380'
+const client = redis.createClient(url);
 
-const redis = require("redis")
-const { default: axios } = require("axios")
-const { stringify } = require("querystring")
-const redisUrl = "redis://127.0.0.1:6379";
-const client = redis.createClient(redisUrl)
+(async () => {
+    client.on('error', err => console.log('Redis Client Error', err));
+})();
 
-client.set = util.promisify(client.set)
-client.get = util.promisify(client.get)
 
-app.post('/', async (req, res) => {
-    const { key, value } = req.body;
-    try {
-        const r = await client.set(key, value);
-        if (r) {
-            res.send({ r })
-        }
-        else {
-            res.send({ message: "data not found" })
-        }
-    } catch (error) {
 
+async function test() {
+    await client.connect();
+
+    // await client.set('1212', 60, '121212');
+    // const value = await client.get('key');
+    // await client.HSET('key', 'field', 'value');
+    // const val = await client.HGETALL('key');
+    // console.log(val.field);
+    const key = "k1";
+    const value = "aman";
+    await client.setEx(key, 10, value);
+    const val = await client.get(key);
+    console.log(val);
+
+
+    // await client.disconnect();
+}
+test();
+
+async function test2() {
+    let setSize = 20;
+
+    await client.sAdd("key", "member1");
+    await client.sAdd("key", "member2");
+
+    while (setSize > 0) {
+        client.sAdd("key", "member" + setSize);
+        setSize -= 1;
     }
-})
-app.get('/', async (req, res) => {
-    const { key } = req.body;
-    try {
-        const data = await client.get(key);
-        if (data) {
-            res.send({ data: data })
-        } else {
-            res.send({ message: "data not found!" })
-        }
 
-
-    } catch (error) {
-        res.send({ error: error })
-    }
-})
-
-app.post('/user/:id', async (req, res) => {
-    const id = req.params.id;
-    const cachedUser = await client.get(`user${id}`);
-    if (cachedUser && typeof (cachedUser) == 'string') {
-        res.json({
-            message: "user already added to redis"
-        })
-    }
-    else {
-        try {
-            const response = await axios({
-                url: `https://jsonplaceholder.typicode.com/users/${id}`,
-                method: "get",
+    // chain commands
+    const x = await client
+        .multi()
+        .sCard("key")
+        .sMembers("key")
+        .keys("*")
+        .dbSize()
+        .exec(function (err, replies) {
+            console.log("MULTI got " + replies.length + " replies");
+            replies.forEach(function (reply, index) {
+                console.log("REPLY  @ index " + index + ": " + reply.toString());
             });
-            const user = response?.data;
-            if (user) {
-                const r = await client.set(`user${id}`, JSON.stringify(user))
-                res.json({
-                    response: r,
-                    user: user,
-                    message: "user added to redis"
-                })
-            }
-            else {
-                res.send({
-                    message: "Data not found"
-                })
-            }
-
-        } catch (error) {
-            res.send({
-                message: "Data not found",
-                error: error
-            })
-        }
-    }
-
-})
-app.get('/user/:id', async (req, res) => {
-    const id = req.params.id;
-    const cachedUser = await client.get(`user${id}`);
-    if (cachedUser && typeof (cachedUser) == 'string') {
-        res.json(
-            {
-                message: "response from redis",
-                user: JSON.parse(cachedUser)
-            }
-        )
-    }
-    else {
-        try {
-            const response = await axios({
-                url: `https://jsonplaceholder.typicode.com/users/${id}`,
-                method: "get",
-            });
-            const user = response?.data;
-            if (user) {
-
-                const r = await client.set(`user${id}`, JSON.stringify(user))
-                res.json({
-                    response: r,
-                    user: user
-                })
-            }
-            else {
-                res.send({ message: "data not found!" })
-            }
-        } catch (error) {
-            res.send({
-                message: "data not found!",
-                error: error
-            })
-        }
-    }
-
-})
-
-app.listen(5000, () => {
-    console.log("server is started at port 5001");
-})
+        });
+    console.log(x);
+}
+test2();
